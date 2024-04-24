ebc04bbbf0d9aee0d0cb0248a20a1590c32590f8
fatih <fsenyigit8@gmail.com>
2024-04-24 16:16:47 +0300

    checkout page

--- src/components/ProductBar.jsx
+++ src/components/ProductBar.jsx
@@ -1,9 +1,33 @@
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { FaCartArrowDown } from "react-icons/fa";
 
 const ProductBar = () => {
   return (
-    <div>ProductBar</div>
-  )
-}
+    <div>
+      <Form className="productBarForm">
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label Label>Product Name</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="price">
+          <Form.Label Label>Product Price</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="quantity">
+          <Form.Label Label>Product Quantity</Form.Label>
+          <Form.Control type="number" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="image">
+          <Form.Label Label>Product Image</Form.Label>
+          <Form.Control type="url" placeholder="https://example.com/" />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          <FaCartArrowDown /> add to cart
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
-export default ProductBar
+export default ProductBar;
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,12 +1,14 @@
-
+import Button from "react-bootstrap/Button";
+import ProductBar from "../components/ProductBar";
 
 const CheckoutPage = () => {
   return (
-    <div>
-      <h1>Checkour Page</h1>
-      
+    <div className="container ">
+      <h1 className="text-center mt-2 text-danger">Checkout Page</h1>
+      <Button className="btn-warning mb-5">hide product bar</Button>
+      <ProductBar/>
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutPage
+export default CheckoutPage;
