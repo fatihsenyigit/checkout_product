932368dfef281636a052a94f255dc8b5901d5dad
fatih <fsenyigit8@gmail.com>
2024-04-27 12:36:58 +0300

    table

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,44 +1,48 @@
 import Table from "react-bootstrap/Table";
 import data from "../helpers/data";
 import Button from "react-bootstrap/Button";
-import "../css/productList.css"
+import "../css/productList.css";
 
 const ProductList = () => {
   return (
     <div className="productList-container">
-      {data.map((e) => {
-        return (
-          <div key={e.id}>
-            <img src="e.image" alt="" />
-            <div>
-              <div>{e.name}</div>
-              <div>{e.price}</div>
-              <div>{e.amount}</div>
-              <Button className="btn-danger">remove</Button>
+      <div className="productListMain">
+        {data.map((e) => {
+          return (
+            <div className="eachProduct" key={e.id}>
+              <img src="e.image" alt="" />
+              <div>
+                <div>{e.name}</div>
+                <div>{e.price}</div>
+                <div>{e.amount}</div>
+                <Button className="btn-danger">remove</Button>
+              </div>
             </div>
-          </div>
-        );
-      })}
-      <Table>
-        <tbody>
-          <tr>
-            <th>Subtotal</th>
-            <td>10</td>
-          </tr>
-          <tr>
-            <th>Tax(18%)</th>
-            <td>20</td>
-          </tr>
-          <tr>
-            <th>Shipping</th>
-            <td>30</td>
-          </tr>
-          <tr>
-            <th>Total</th>
-            <td>40</td>
-          </tr>
-        </tbody>
-      </Table>
+          );
+        })}
+      </div>
+      <div className="productListTable">
+        <Table>
+          <tbody>
+            <tr>
+              <th>Subtotal</th>
+              <td className="text-end">1sdfsd088</td>
+            </tr>
+            <tr>
+              <th>Tax(18%)</th>
+              <td className="text-end">20</td>
+            </tr>
+            <tr>
+              <th>Shipping</th>
+              <td className="text-end">30987654345</td>
+            </tr>
+            <tr>
+              <th>Total</th>
+              <td className="text-end">40</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
