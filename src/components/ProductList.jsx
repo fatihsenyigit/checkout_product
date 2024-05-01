c78c4491a59bd050de843f9479e4752e28ec6919
fatih <fsenyigit8@gmail.com>
2024-05-01 11:33:05 +0300

    product list done with all functions

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -25,75 +25,93 @@ const ProductList = () => {
     const total = Number((subTotal+ tax+ shipping).toFixed(2))
 
     const handleRemove = (item) => {
-      console.log(item)
+      setNewData(newData.filter((info)=> info!==item))
     }
   
 
   return (
     <div className="productList-container">
-      <div className="productListMain">
-        {newData.map((e) => {
-          return (
-            <div className="d-flex flex-column gap-1 eachProduct" key={e.id}>
-              <div
-                className="d-flex justify-content-center
+      {newData.length === 0 ? (
+        "there is no product to show"
+      ) : (
+        <>
+          <div className="productListMain">
+            {newData.map((e) => {
+              return (
+                <div
+                  className="d-flex flex-column gap-1 eachProduct"
+                  key={e.id}
+                >
+                  <div
+                    className="d-flex justify-content-center
               "
-              >
-                <img src={image1} alt="" />
-              </div>
+                  >
+                    <img src={image1} alt="" />
+                  </div>
 
-              <div>{e.name}</div>
-              <div className="d-flex align-items-baseline gap-3">
-                <h5 className="text-warning ">${(e.price * 0.8).toFixed(2)}</h5>
-                <h6 className=" text-decoration-line-through">${e.price}</h6>
-              </div>
-              <h5 className="d-flex jus align-items-center gap-2">
-                <FaRegSquareMinus
-                  onClick={() => handleAmount(e.id, -1)}
-                  className=" text-success btnAmount"
-                />
-                {e.amount}
-                <FaRegPlusSquare
-                  onClick={() => handleAmount(e.id, 1)}
-                  className=" text-success btnAmount"
-                />
-              </h5>
+                  <div>{e.name}</div>
+                  <div className="d-flex align-items-baseline gap-3">
+                    <h5 className="text-warning ">
+                      ${(e.price * 0.8).toFixed(2)}
+                    </h5>
+                    <h6 className=" text-decoration-line-through">
+                      ${e.price}
+                    </h6>
+                  </div>
+                  <h5 className="d-flex jus align-items-center gap-2">
+                    <FaRegSquareMinus
+                      onClick={() => handleAmount(e.id, -1)}
+                      className=" text-success btnAmount"
+                    />
+                    {e.amount}
+                    <FaRegPlusSquare
+                      onClick={() => handleAmount(e.id, 1)}
+                      className=" text-success btnAmount"
+                    />
+                  </h5>
 
-              <div className="d-flex justify-content-center">
-                <Button className="btn-danger d-flex gap-2 align-items-center">
-                  <FaRegTrashAlt onClick={()=>handleRemove(e)} />
-                  remove
-                </Button>
-              </div>
-              <div>
-                <p>Product Total: ${(e.price * 0.8 * e.amount).toFixed(2)}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="productListTable">
-        <Table>
-          <tbody>
-            <tr>
-              <th>Subtotal</th>
-              <td className="text-end">${subTotal}</td>
-            </tr>
-            <tr>
-              <th>Tax(18%)</th>
-              <td className="text-end">${tax}</td>
-            </tr>
-            <tr>
-              <th>Shipping</th>
-              <td className="text-end">${shipping}</td>
-            </tr>
-            <tr>
-              <th>Total</th>
-              <td className="text-end">${total}</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+                  <div className="d-flex justify-content-center">
+                    <Button
+                      className="btn-danger d-flex gap-2 align-items-center"
+                      onClick={() => handleRemove(e)}
+                    >
+                      <FaRegTrashAlt />
+                      remove
+                    </Button>
+                  </div>
+                  <div>
+                    <p>
+                      Product Total: ${(e.price * 0.8 * e.amount).toFixed(2)}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="productListTable">
+            <Table>
+              <tbody>
+                <tr>
+                  <th>Subtotal</th>
+                  <td className="text-end">${subTotal}</td>
+                </tr>
+                <tr>
+                  <th>Tax(18%)</th>
+                  <td className="text-end">${tax}</td>
+                </tr>
+                <tr>
+                  <th>Shipping</th>
+                  <td className="text-end">${shipping}</td>
+                </tr>
+                <tr>
+                  <th>Total</th>
+                  <td className="text-end">${total}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </>
+      )}
     </div>
   );
 };
